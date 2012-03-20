68654c87464734a54e8b3b3cf77ae1553450431a
Daniel Harmsworth <atrophy@caffeinator.net>
2012-03-20 20:26:46 +0800

    Added basic controls

--- gen/au/edu/wa/challenger/biobike/R.java
+++ gen/au/edu/wa/challenger/biobike/R.java
@@ -14,12 +14,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int button1=0x7f050006;
+        public static final int button2=0x7f05000b;
+        public static final int button3=0x7f05000c;
+        public static final int editText1=0x7f05000e;
         public static final int relativeLayout2=0x7f050000;
+        public static final int seekBar1=0x7f05000d;
         public static final int tableLayout1=0x7f050001;
+        public static final int tableLayout2=0x7f050009;
         public static final int tableRow1=0x7f050002;
         public static final int tableRow2=0x7f050005;
-        public static final int tableRow3=0x7f050006;
-        public static final int tableRow4=0x7f050007;
+        public static final int tableRow3=0x7f050007;
+        public static final int tableRow4=0x7f050008;
+        public static final int tableRow5=0x7f05000a;
+        public static final int tableRow6=0x7f05000f;
+        public static final int tableRow7=0x7f050010;
+        public static final int tableRow8=0x7f050011;
         public static final int textView1=0x7f050004;
         public static final int toggleButton1=0x7f050003;
     }
