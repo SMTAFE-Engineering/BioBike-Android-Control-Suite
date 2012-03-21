3d2714dc4755c7bf694e078af09e87f3414331d9
Daniel Harmsworth <atrophy@caffeinator.net>
2012-03-21 14:01:58 +0800

    UI code now in a workable state

--- gen/au/edu/wa/challenger/biobike/R.java
+++ gen/au/edu/wa/challenger/biobike/R.java
@@ -14,24 +14,40 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050006;
-        public static final int button2=0x7f05000b;
-        public static final int button3=0x7f05000c;
-        public static final int editText1=0x7f05000e;
+        public static final int btn_hba_down=0x7f05000b;
+        public static final int btn_hba_up=0x7f05000c;
+        public static final int btn_hbl_down=0x7f050011;
+        public static final int btn_hbl_up=0x7f050012;
+        public static final int btn_spa_down=0x7f050017;
+        public static final int btn_spa_up=0x7f050018;
+        public static final int btn_spl_down=0x7f05001d;
+        public static final int btn_spl_up=0x7f05001e;
+        public static final int button1=0x7f050004;
         public static final int relativeLayout2=0x7f050000;
-        public static final int seekBar1=0x7f05000d;
+        public static final int skb_hba=0x7f05000d;
+        public static final int skb_hbl=0x7f050013;
+        public static final int skb_spa=0x7f050019;
+        public static final int skb_spl=0x7f05001f;
         public static final int tableLayout1=0x7f050001;
         public static final int tableLayout2=0x7f050009;
-        public static final int tableRow1=0x7f050002;
-        public static final int tableRow2=0x7f050005;
+        public static final int tableRow1=0x7f050003;
+        public static final int tableRow2=0x7f050006;
         public static final int tableRow3=0x7f050007;
         public static final int tableRow4=0x7f050008;
         public static final int tableRow5=0x7f05000a;
-        public static final int tableRow6=0x7f05000f;
-        public static final int tableRow7=0x7f050010;
-        public static final int tableRow8=0x7f050011;
-        public static final int textView1=0x7f050004;
-        public static final int toggleButton1=0x7f050003;
+        public static final int tableRow6=0x7f050010;
+        public static final int tableRow7=0x7f050016;
+        public static final int tableRow8=0x7f05001c;
+        public static final int textView1=0x7f050002;
+        public static final int textView2=0x7f050005;
+        public static final int tog_hba_enable=0x7f05000f;
+        public static final int tog_hbl_enable=0x7f050015;
+        public static final int tog_spa_enable=0x7f05001b;
+        public static final int tog_spl_enable=0x7f050021;
+        public static final int txt_hba_pos=0x7f05000e;
+        public static final int txt_hbl_pos=0x7f050014;
+        public static final int txt_spa_pos=0x7f05001a;
+        public static final int txt_spl_pos=0x7f050020;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/au/edu/wa/challenger/biobike/BioBikeActivity.java
+++ src/au/edu/wa/challenger/biobike/BioBikeActivity.java
@@ -2,12 +2,159 @@ package au.edu.wa.challenger.biobike;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.*;
 
 public class BioBikeActivity extends Activity {
+	public int hba_loc;
+	Button			hba_btn_up;
+	Button			hba_btn_down;
+	EditText 		hba_loc_txt;
+	SeekBar			hba_loc_skb;
+	ToggleButton	hba_loc_ena;
+	
+	int hbl_loc = 50;
+	Button			hbl_btn_up;
+	Button			hbl_btn_down;
+	EditText 		hbl_loc_txt;
+	SeekBar			hbl_loc_skb;
+	ToggleButton	hbl_loc_ena;
+	
+	int spa_loc = 50;
+	Button			spa_btn_up;
+	Button			spa_btn_down;
+	EditText 		spa_loc_txt;
+	SeekBar			spa_loc_skb;
+	ToggleButton	spa_loc_ena;
+	
+	int spl_loc = 50;
+	Button			spl_btn_up;
+	Button			spl_btn_down;
+	EditText 		spl_loc_txt;
+	SeekBar			spl_loc_skb;
+	ToggleButton	spl_loc_ena;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //This is where the ADK stuff should retrieve the current actuator positions.
+        hba_loc_txt = (EditText) findViewById(R.id.txt_hba_pos);
+        hba_loc_skb = (SeekBar) findViewById(R.id.skb_hba);
+    	hba_loc_ena = (ToggleButton) findViewById(R.id.tog_hba_enable);
+    	hba_btn_up = (Button) findViewById(R.id.btn_hba_up);
+    	hba_btn_down = (Button) findViewById(R.id.btn_hba_down);
+    	
+    	hbl_loc_txt = (EditText) findViewById(R.id.txt_hbl_pos);
+        hbl_loc_skb = (SeekBar) findViewById(R.id.skb_hbl);
+    	hbl_loc_ena = (ToggleButton) findViewById(R.id.tog_hbl_enable);
+    	hbl_btn_up = (Button) findViewById(R.id.btn_hbl_up);
+    	hbl_btn_down = (Button) findViewById(R.id.btn_hbl_down);
+
+    	spa_loc_txt = (EditText) findViewById(R.id.txt_spa_pos);
+        spa_loc_skb = (SeekBar) findViewById(R.id.skb_spa);
+    	spa_loc_ena = (ToggleButton) findViewById(R.id.tog_spa_enable);
+    	spa_btn_up = (Button) findViewById(R.id.btn_spa_up);
+    	spa_btn_down = (Button) findViewById(R.id.btn_spa_down);
+    	
+    	spl_loc_txt = (EditText) findViewById(R.id.txt_spl_pos);
+        spl_loc_skb = (SeekBar) findViewById(R.id.skb_spl);
+    	spl_loc_ena = (ToggleButton) findViewById(R.id.tog_spl_enable);
+    	spl_btn_up = (Button) findViewById(R.id.btn_spl_up);
+    	spl_btn_down = (Button) findViewById(R.id.btn_spl_down);
+
+    	
+    	set_hba(50);
+    	set_hbl(50);
+    	set_spa(50);
+    	set_spl(50);
+        
+    	// Setting up the seekbar listeners
+        hba_loc_skb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+        	public void onProgressChanged(SeekBar seekbar_obj, int pos, boolean fromuser) {
+        		if (fromuser) { set_hba(pos); }
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) { /*Nothing to see here*/ }
+            public void onStopTrackingTouch(SeekBar seekBar) { /*Nothing to see here*/ }
+        } );
+        
+        hbl_loc_skb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+        	public void onProgressChanged(SeekBar seekbar_obj, int pos, boolean fromuser) {
+        		if (fromuser) { set_hbl(pos); }
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) { /*Nothing to see here*/ }
+            public void onStopTrackingTouch(SeekBar seekBar) { /*Nothing to see here*/ }
+        } );
+        
+        spa_loc_skb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+        	public void onProgressChanged(SeekBar seekbar_obj, int pos, boolean fromuser) {
+        		if (fromuser) { set_spa(pos); }
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) { /*Nothing to see here*/ }
+            public void onStopTrackingTouch(SeekBar seekBar) { /*Nothing to see here*/ }
+        } );
+        
+        spl_loc_skb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { 
+        	public void onProgressChanged(SeekBar seekbar_obj, int pos, boolean fromuser) {
+        		if (fromuser) { set_spl(pos); }
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) { /*Nothing to see here*/ }
+            public void onStopTrackingTouch(SeekBar seekBar) { /*Nothing to see here*/ }
+        } );
+        
     }
+    
+    private void set_hba(int pos)
+    {
+    	hba_loc = pos;
+    	hba_loc_txt.setText(Integer.toString(hba_loc));
+    	hba_loc_skb.setProgress(hba_loc);
+    	
+    	//ADK call to set the HBA actuator position goes here.
+    }
+    
+    private void set_hbl(int pos)
+    {
+    	hbl_loc = pos;
+    	hbl_loc_txt.setText(Integer.toString(hbl_loc));
+    	hbl_loc_skb.setProgress(hbl_loc);
+    	
+    	//ADK call to set the HBL actuator position goes here.
+    }
+    
+    private void set_spa(int pos)
+    {
+    	spa_loc = pos;
+    	spa_loc_txt.setText(Integer.toString(spa_loc));
+    	spa_loc_skb.setProgress(spa_loc);
+    	
+    	//ADK call to set the SPA actuator position goes here.
+    }
+    
+    private void set_spl(int pos)
+    {
+    	spl_loc = pos;
+    	spl_loc_txt.setText(Integer.toString(spl_loc));
+    	spl_loc_skb.setProgress(spl_loc);
+    	
+    	//ADK call to set the SPL actuator position goes here.
+    }
+
+    // Button Handlers Ahoy!
+    public void upClick_hba (View v){	set_hba(hba_loc + 1); }
+    public void downClick_hba (View v){	set_hba(hba_loc - 1); }
+    
+    public void upClick_hbl (View v){	set_hbl(hbl_loc + 1); }
+    public void downClick_hbl (View v){	set_hbl(hbl_loc - 1); }
+    
+    public void upClick_spa (View v){	set_spa(spa_loc + 1); }
+    public void downClick_spa (View v){	set_spa(spa_loc - 1); }
+    
+    public void upClick_spl (View v){ 	set_spl(spl_loc + 1); }
+    public void downClick_spl (View v){	set_spl(spl_loc - 1); }
+
+
 }
